7efe877cf4c12caf0c83687b35a6f875426fb364
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-20 15:50:50 -0700

    Pull in app.js from lab-18, remove socket.io code and replace with message queue client server

---
+++ app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+require('dotenv').config();
+
+const fs = require('fs');
+const Q = require('@nmq/q/client');
+
+const alterFile = (file) => {
+  readFileWrapper(file)
+    .then(data => {
+      writeFileWrapper(file, caps(data));
+    })
+    .then((file) => {
+      let message = {
+        name: 'save',
+        message: `File saved!`,
+      };
+
+      emitFileSave(message);
+    })
+    .catch(() => {
+      let error = {
+        name: 'error',
+        message: 'ERROR: something went wrong',
+      };
+      Q.publish('files', 'error', JSON.stringify(error));
+    });
+};
+
+let readFileWrapper = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile( file, (err, data) => {
+      if(err) { 
+        reject(err); 
+      } else {
+        resolve(data.toString());
+      }
+    });
+  });
+};
+
+let caps = (data) => {
+  return data.toUpperCase();
+};
+
+let emitFileSave = (payload) => {
+  Q.publish('files', 'save', JSON.stringify(payload));
+};
+
+let writeFileWrapper = (file, text) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile( file, Buffer.from(text), (err) => {
+      if(err) {
+        reject(err); 
+      } else {
+        resolve(file);
+      }
+    });
+  });
+};
+
+let file = process.argv.slice(2).shift();
+alterFile(file);
+
+module.exports = exports = {readFileWrapper, caps, writeFileWrapper};
+
+
